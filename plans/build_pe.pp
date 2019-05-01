1280d76e56d0e7eedfd87b5ae74adb4b0c4ae502
barriserloth <barriserloth@gmail.com>
2019-05-01 10:03:58 -0700

    Add pe-tasks, pe-backup-tools packages to build_pe plan
    This commit adds the last two vanagon packages currently supported for
    the build_pe plan. There's a lot of duplicate code in this plan right
    now, and outside of building the parameter hashes for each package, the
    `check_parameters` calls and the following if-elsif blocks can probably
    be consolidated into a separate plan that gets called from this one,
    which I imagine would reduce the amount of code duplication currently
    happening here. Ideally, I'll be doing that in the next commit (or, you
    know, relatively soon^TM)

--- plans/build_pe.pp
+++ plans/build_pe.pp
@@ -77,6 +77,44 @@ plan localbuilder::build_pe(
   elsif $pr_changes_installer {
     run_task(localbuilder::build_vanagon_package, localhost, platforms => [$platform], version => $version, vanagon_project => 'pe-installer-vanagon', vanagon_component_prs => $pe_installer_pr_params)
   }
+
+###########################################
+# Build pe-backup-tools if changes provided
+###########################################
+  $local_pe_backup_tools_params = { 'pe-backup-tools' => $pe_backup_tools }
+  $pe_backup_tools_pr_params = { 'pe-backup-tools' => $pe_backup_tools_pr }
+
+  $local_changes_backup_tools = run_task(localbuilder::check_parameters, localhost, parameters_hash => $local_pe_backup_tools_params).first().value()['changes_present']
+  $pr_changes_backup_tools = run_task(localbuilder::check_parameters, localhost, parameters_hash => $pe_backup_tools_pr_params).first().value()['changes_present']
+
+  if $local_changes_backup_tools and $pr_changes_backup_tools {
+    run_task(localbuilder::build_vanagon_package, localhost, platforms => [$platform], version => $version, vanagon_project => 'pe-backup-tools-vanagon', local_vanagon_components => $local_pe_backup_tools_params, vanagon_component_prs => $pe_backup_tools_pr_params)
+  }
+  elsif $local_changes_backup_tools {
+    run_task(localbuilder::build_vanagon_package, localhost, platforms => [$platform], version => $version, vanagon_project => 'pe-backup-tools-vanagon', local_vanagon_components => $local_pe_backup_tools_params)
+  }
+  elsif $pr_changes_backup_tools {
+    run_task(localbuilder::build_vanagon_package, localhost, platforms => [$platform], version => $version, vanagon_project => 'pe-backup-tools-vanagon', vanagon_component_prs => $pe_backup_tools_pr_params)
+  }
+
+####################################
+# Build pe-tasks if changes provided
+####################################
+  $local_pe_tasks_params = { 'puppetlabs-facter_task' => $facter_task, 'puppetlabs-package' => $package, 'puppetlabs-puppet_conf' => $puppet_conf, 'puppetlabs-pe_installer_cd4pe' => $pe_installer_cd4pe, 'puppetlabs-service' => $service }
+  $pe_tasks_pr_params = { 'puppetlabs-facter_task' => $facter_task_pr, 'puppetlabs-package' => $package_pr, 'puppetlabs-puppet_conf' => $puppet_conf_pr, 'puppetlabs-pe_installer_cd4pe' => $pe_installer_cd4pe_pr, 'puppetlabs-service' => $service_pr }
+
+  $local_changes_tasks = run_task(localbuilder::check_parameters, localhost, parameters_hash => $local_pe_installer_params).first().value()['changes_present']
+  $pr_changes_tasks = run_task(localbuilder::check_parameters, localhost, parameters_hash => $pe_installer_pr_params).first().value()['changes_present']
+
+  if $local_changes_tasks and $pr_changes_tasks {
+    run_task(localbuilder::build_vanagon_package, localhost, platforms => [$platform], version => $version, vanagon_project => 'pe-tasks', local_vanagon_components => $local_pe_tasks_params, vanagon_component_prs => $pe_tasks_pr_params)
+  }
+  elsif $local_changes_tasks {
+    run_task(localbuilder::build_vanagon_package, localhost, platforms => [$platform], version => $version, vanagon_project => 'pe-tasks', local_vanagon_components => $local_pe_tasks_params)
+  }
+  elsif $pr_changes_tasks {
+    run_task(localbuilder::build_vanagon_package, localhost, platforms => [$platform], version => $version, vanagon_project => 'pe-tasks', vanagon_component_prs => $pe_tasks_pr_params)
+  }
   
 ########################################
 # Provision VM and build PE tarball
